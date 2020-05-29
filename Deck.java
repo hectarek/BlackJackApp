2daccc4b6fe2449435d9659bbad9309a044c4f31
larell mcbride <mcbridelarell@gmail.com>
2020-05-29 15:44:10 -0400

    finished Deck

--- Deck.java
+++ Deck.java
@@ -73,8 +73,9 @@ public void shuffle() {
         public void printDeck(int numToPrint) {
 
             for (int c =0; c < numToPrint; c++) {
-                
+                System.out.printf("% 3d/%d %s \n", c+1, this.numCards,this.myCards[c].toString());
             }
+            System.out.printf("\t\t[%d other\n", this.numCards-numToPrint);
         }
     
 }
